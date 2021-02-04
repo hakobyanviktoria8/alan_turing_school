e26169178d1365cbaff1e2e6cf116497e5fb1889
Viktorya Hakobyan <hakobyanviktorya8@gmail.com>
2021-02-04 23:39:35 +0400

    Sing up bg

--- src/SignUp/SignUp.js
+++ src/SignUp/SignUp.js
@@ -1,20 +1,22 @@
 import React from "react";
 import { Container, Row, Col } from 'reactstrap';
 import "./SignUp.css";
-import bgImg from "./img/bgSignUp.svg";
-// import bg from "./img/Rectangle 1996.svg";
+// import logoBig from "img/logoBig.png";
 
 export function SignUP(props){
     return(
         <Container fluid className={"SignUP"}>
             <Row>
                 <Col lg={"7"} className={"border p-0"}>
-                    <div  style={{
-                        backgroundImage: `url(${bgImg})`,
-                        width: "100%",
-                        height: "100vh",
-                    }}
-                          className={"bgSignUp"}> </div>
+                    <div  className={"bgSignUp"}>
+                        {/*<img src={logoBig} alt=""/>*/}
+                        <div className={"header-left"}>
+                            <h1>Lorem Ipsum </h1>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                                Consequuntur ex officia officiis possimus ut. Soluta.
+                            </p>
+                        </div>
+                    </div>
                 </Col>
                 <Col lg={"5"} className={"border"}> </Col>
             </Row>
