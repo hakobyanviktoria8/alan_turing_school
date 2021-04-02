86ebc94215d408d4b580bdffb1f14d1c4e613604
Viktorya Hakobyan <hakobyanviktorya8@gmail.com>
2021-04-02 19:33:56 +0400

    Main page start

--- src/MainPage/MainPage.js
+++ src/MainPage/MainPage.js
@@ -1,21 +1,16 @@
 import React from "react";
 import "../SignUp/SignUp.css";
-import {useLocation} from "react-router-dom";
 import SidebarLeft from "./SidebarLeft/SidebarLeft";
 import SidebarTop from "./SidebarTop/SidebarTop";
 import MainContent from "./MainContent/MainContent";
-import SidebarRight from "./SidebarRight/SidebarRight";
 
 export function MainPage(props) {
-    let location = useLocation();
-    let edu_title = location.pathname.split("/")[2] === "education";
 
     return (
         <div className={"MainPage"}>
             <SidebarTop/>
             <SidebarLeft/>
             <MainContent/>
-            <SidebarRight edu_arrow={edu_title}/>
         </div>
     )
 }
